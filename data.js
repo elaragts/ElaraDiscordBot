e2c8959c9d00ad6aafc10dd0ec621f54d9aff42e
FlappyBox <ajandke1999@gmail.com>
2024-02-18 18:31:43 -0500

    Fixed line indentation.

--- data.js
+++ data.js
@@ -11,32 +11,32 @@ const songs = {}; //uniqueId: {id, titles: [jp, en]}
 
 //create song object
 const getMusicInfoFromId = (id) => {
-  for (let i in musicinfo.items) {
-      if (musicinfo.items[i].id === id) {
-          return [
-              musicinfo.items[i].uniqueId,
-              [
-                  musicinfo.items[i].starEasy,
-                  musicinfo.items[i].starNormal,
-                  musicinfo.items[i].starHard,
-                  musicinfo.items[i].starMania,
-                  musicinfo.items[i].starUra,
-              ],
-              musicinfo.items[i].genreNo
-          ]
-      }
-  }
-  return [-1,[0,0,0,0,0],-1]; //probably dumb
+    for (let i in musicinfo.items) {
+        if (musicinfo.items[i].id === id) {
+            return [
+                musicinfo.items[i].uniqueId,
+                [
+                    musicinfo.items[i].starEasy,
+                    musicinfo.items[i].starNormal,
+                    musicinfo.items[i].starHard,
+                    musicinfo.items[i].starMania,
+                    musicinfo.items[i].starUra,
+                ],
+                musicinfo.items[i].genreNo
+            ]
+        }
+    }
+    return [-1, [0, 0, 0, 0, 0], -1]; //probably dumb
 }
 
 //create song object
 for (let i in wordlist.items) {
-  if (wordlist.items[i].key.startsWith('song') && !wordlist.items[i].key.startsWith('song_sub') && !wordlist.items[i].key.startsWith('song_detail')) {
-      const id = wordlist.items[i].key.slice(5); //remove song_ from id
-      const [uniqueId, difficulty, genreNo] = getMusicInfoFromId(id);
-      if (uniqueId in songs) continue;
-      songs[uniqueId] = {'id': id, titles : [wordlist.items[i].japaneseText, wordlist.items[i].englishUsText], 'difficulty' : difficulty, 'genreNo' : genreNo };
-  }
+    if (wordlist.items[i].key.startsWith('song') && !wordlist.items[i].key.startsWith('song_sub') && !wordlist.items[i].key.startsWith('song_detail')) {
+        const id = wordlist.items[i].key.slice(5); //remove song_ from id
+        const [uniqueId, difficulty, genreNo] = getMusicInfoFromId(id);
+        if (uniqueId in songs) continue;
+        songs[uniqueId] = { 'id': id, titles: [wordlist.items[i].japaneseText, wordlist.items[i].englishUsText], 'difficulty': difficulty, 'genreNo': genreNo };
+    }
 }
 
 /**
@@ -47,7 +47,7 @@ const searchSongs = (query) => {
     query = query.toLowerCase();
     for (let song in songs) {
         for (let i in songs[song].titles) {
-            if (songs[song].titles[i].toLowerCase() === query) return [song,i]; //return the song if there is an exact match
+            if (songs[song].titles[i].toLowerCase() === query) return [song, i]; //return the song if there is an exact match
         }
     }
     return [];
@@ -59,7 +59,7 @@ const searchSongs = (query) => {
  * @returns {Promise<string[]>}
  */
 const autocomplete = async (query) => {
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         try {
             if (query === '') resolve([]);
             query = query.toLowerCase();
@@ -97,51 +97,48 @@ const getSongName = (uniqueId, lang) => {
 }
 
 const getSongStars = (uniqueId, difficulty) => {
-  if (!(uniqueId in songs)) throw new Error("Song not found!");
-  return songs[uniqueId].difficulty[difficulty - 1];
+    if (!(uniqueId in songs)) throw new Error("Song not found!");
+    return songs[uniqueId].difficulty[difficulty - 1];
 }
 
 const getEventSongs = (folderId) => {
-  for (let i of eventfolderdata) {
-    if (i.folderId == folderId) {
-      return i.songNo;
+    for (let i of eventfolderdata) {
+        if (i.folderId == folderId) {
+            return i.songNo;
+        }
     }
-  }
-  throw new Error("Folder not found!");
+    throw new Error("Folder not found!");
 }
 
 const getEventFromSong = (uniqueId) => {
-  if (!(uniqueId in songs)) throw new Error("Song not found!");
-  let folder = -1;
-  for (let i in eventfolderdata) {
-    if (isSongInEvent(uniqueId, eventfolderdata[i].folderId)) folder = eventfolderdata[i].folderId;
-  }
+    if (!(uniqueId in songs)) throw new Error("Song not found!");
 
-  //Priority event folders
-  if (isSongInEvent(uniqueId, 10)) folder = 10;
-  else if (isSongInEvent(uniqueId, 8)) folder = 8;
-  else if (isSongInEvent(uniqueId, 9)) folder = 9;
-  else if (isSongInEvent(uniqueId, 3)) folder = 3;
-  
-  return folder;
+
+    //Priority event folders
+    if (isSongInEvent(uniqueId, 10)) folder = 10;
+    else if (isSongInEvent(uniqueId, 8)) folder = 8;
+    else if (isSongInEvent(uniqueId, 9)) folder = 9;
+    else if (isSongInEvent(uniqueId, 3)) folder = 3;
+
+    return folder;
 }
 
 //Converts folder id to name with language.
 const folderIdToName = (folderId, lang) => {
-  if (!isEventFolderPresent) throw new Error("Event folder not found!");
-  if (!isLangInRange(lang)) throw new Error("Lang out of range!");
-  for (let i of eventfolderdata) {
-    if (i.folderId === folderId) {
-      for (let j of wordlist.items) {
-        if (j.key === "folder_event" + folderId) {
-          switch (lang) {
-            case 0: return j.japaneseText;
-            case 1: return j.englishUsText;
-          }
+    if (!isEventFolderPresent) throw new Error("Event folder not found!");
+    if (!isLangInRange(lang)) throw new Error("Lang out of range!");
+    for (let i of eventfolderdata) {
+        if (i.folderId === folderId) {
+            for (let j of wordlist.items) {
+                if (j.key === "folder_event" + folderId) {
+                    switch (lang) {
+                        case 0: return j.japaneseText;
+                        case 1: return j.englishUsText;
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
 
 /**
@@ -159,25 +156,25 @@ const isSongPresent = (uniqueId) => {
  * @returns {boolean}
  */
 const isLangInRange = (lang) => {
-    return lang >= 0 && lang <=1 ;
+    return lang >= 0 && lang <= 1;
 }
 
 const isSongInEvent = (uniqueId, folderId) => {
-  if (!(uniqueId in songs)) throw new Error("Song not found!");
-  if (!isEventFolderPresent) throw new Error("Event folder not found!");
-  let index = 0;
-  for (let i in eventfolderdata) {
-    if (eventfolderdata[i].folderId === folderId) index = i;
-  }
+    if (!(uniqueId in songs)) throw new Error("Song not found!");
+    if (!isEventFolderPresent) throw new Error("Event folder not found!");
+    let index = 0;
+    for (let i in eventfolderdata) {
+        if (eventfolderdata[i].folderId === folderId) index = i;
+    }
 
-  return eventfolderdata[index].songNo.includes(parseInt(uniqueId));
+    return eventfolderdata[index].songNo.includes(parseInt(uniqueId));
 }
 
 const isEventFolderPresent = (folderId) => {
-  for (let i of eventfolderdata) {
-    if (i.folderId === folderId) return true;
-  }
+    for (let i of eventfolderdata) {
+        if (i.folderId === folderId) return true;
+    }
 
-  return false;
+    return false;
 }
 module.exports = { searchSongs, autocomplete, getSongName, isSongPresent, isLangInRange, getSongStars, getEventSongs, isSongInEvent, getEventFromSong, folderIdToName };
