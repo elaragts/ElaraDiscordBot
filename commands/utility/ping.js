91e944f05351b44d5df009bebde15bfd6bc90fce
Aiden Jandke <ajandke1999@gmail.com>
2024-02-17 21:56:51 -0500

    Reverted ping.

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,16 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
-const data = require('@data');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        let eventSongs = data.getEventSongs(1);
-        let message = '';
-        for (let i of eventSongs) {
-          message += data.getSongName(i, 1) + '\n';
-        }
-        await interaction.reply(message);
+        await interaction.reply('Pong!');
     },
 };
