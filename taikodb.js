23438cb9604b329e056635e7358d57c3a6092946
FlappyBox <ajandke1999@gmail.com>
2024-02-20 22:37:34 -0500

    Changes bestscore so it would show best crown.

--- taikodb.js
+++ taikodb.js
@@ -10,17 +10,22 @@ const db = new Database('./taiko.db3', { readonly: true });
 //statements
 const lb = db.prepare('SELECT ud.MyDonName, c.AccessCode, sbd.BestScore, sbd.BestCrown, sbd.BestScoreRank FROM SongBestData sbd INNER JOIN UserData ud ON sbd.Baid = ud.Baid INNER JOIN Card c ON sbd.Baid = c.Baid WHERE SongID = ? AND Difficulty = ? ORDER BY sbd.BestScore DESC LIMIT 10')
 const selectBaidFromAccessCode = db.prepare('SELECT Baid FROM Card WHERE AccessCode = ?').pluck();
-const selectBestScore = db.prepare('SELECT BestScore FROM SongBestData WHERE SongID = ? AND Difficulty = ? AND Baid = ?').pluck();
+const selectBestScore = db.prepare('SELECT BestScore, BestCrown FROM SongBestData WHERE SongID = ? AND Difficulty = ? AND Baid = ?');
 const selectPlayByScore = db.prepare('SELECT ud.MyDonName, spd.PlayTime, spd.Score, spd.ComboCount, spd.Crown, spd.ScoreRank, spd.DrumrollCount, spd.GoodCount, spd.MissCount, spd.OkCount, c.AccessCode FROM SongPlayData spd INNER JOIN UserData ud ON spd.Baid = ud.Baid INNER JOIN Card c ON spd.Baid = c.baid WHERE spd.SongID = ? AND spd.Difficulty = ? AND spd.Baid = ? AND spd.Score = ? ORDER BY spd.Id')
 const getLeaderboard = (uniqueId, difficulty) => {
     return lb.all(uniqueId, difficulty);
 }
 
 const getBestScore = (uniqueId, difficulty, Baid) => {
-    const score = selectBestScore.get(uniqueId, difficulty, Baid);
+    const score = selectBestScore.get(uniqueId, difficulty, Baid).BestScore;
     if (score === undefined) return undefined;
     return selectPlayByScore.get(uniqueId, difficulty, Baid, score);
 }
+
+const getBestCrown = (uniqueId, difficulty, Baid) => {
+    return selectBestScore.get(uniqueId, difficulty, Baid).BestCrown;
+}
+
 const getBaidFromAccessCode = (accessCode) => {
     return selectBaidFromAccessCode.get(accessCode);
 }
@@ -62,4 +67,4 @@ const difficultyIdToName = (difficultyId, lang) => {
             throw new Error(`Unknown language ${lang}`);
     }
 }
-module.exports = { getLeaderboard, getBaidFromAccessCode, difficultyIdToName, getBestScore };
+module.exports = { getLeaderboard, getBaidFromAccessCode, difficultyIdToName, getBestScore, getBestCrown };
