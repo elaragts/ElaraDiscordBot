c2802b9224a5fda74b2d932ca647f237aabfa963
keitannunes <keitannunes@gmail.com>
2024-03-16 16:59:02 -0400

    Deployslashcommands runs on all servers

--- deploy_slashcommands.js
+++ deploy_slashcommands.js
@@ -1,6 +1,6 @@
 require('module-alias/register');
-const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const {REST, Routes} = require('discord.js');
+const {clientId, guildId, token} = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
 
@@ -34,10 +34,8 @@ const rest = new REST().setToken(token);
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+        const data = await rest.put(Routes.applicationCommands(clientId), {body: commands})
+
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
